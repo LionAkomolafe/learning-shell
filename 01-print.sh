9144d00d49e477e6ab153fd2540c01528216fb63
Yemi <tywonderkid@yahoo.com>
2023-01-20 12:41:07 -0600

    print script

--- 01-print.sh
+++ 01-print.sh
@@ -1,3 +1,9 @@
-ls
-cat /etc/passwd
-find / -name shadow
+echo Hello
+# Color Syntax
+# echo -e "\e[COLmHello\e0m"
+# -e - enable \e
+# \e - enable COL
+# COL - COL CODE, 31-36 (31 - Red, 32 - Green, 33 - Yellow, 34 - Blue, 35 - Magenta, 36 - Cyan)
+# 0m - disables the enabled color
+
+echo "\e[31mHello in RED Color\e[0m"
