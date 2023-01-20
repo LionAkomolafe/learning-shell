83e14d59b9172617438a95010b3f747da99085ce
Yemi <tywonderkid@yahoo.com>
2023-01-19 19:25:55 -0600

    vars script

--- 02-vars.sh
+++ 02-vars.sh
@@ -1,1 +1,8 @@
-echo Hello
+## Variable - Variables are nothing but a name that is assigned to a set of data
+
+## Declare a variable
+#VARNAME=DATA (NO spaces)
+a=10
+
+## Access a variable - $VARNAME
+A - $a
