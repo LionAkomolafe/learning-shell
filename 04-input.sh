a4d9502a739e6e932ff59912402102e9e96cfa7c
Yemi <tywonderkid@yahoo.com>
2023-01-20 15:14:12 -0600

    input script

--- 04-input.sh
+++ 04-input.sh
@@ -1,4 +1,7 @@
-read -p "Please Enter MySQL Username: " user
-read -s -p  "Please enter a Password: " password
+#read -p "Please Enter MySQL Username: " user
+#read -s -p  "Please enter a Password: " password
 
-echo "Username & Password from read = $user & $password"
+#echo "Username & Password from read = $user & $password"
+
+echo Username = $1
+echo Password = $2
