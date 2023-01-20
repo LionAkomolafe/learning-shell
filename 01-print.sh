148702d29976404de3566d3a18531a2a8c20188d
Yemi <tywonderkid@yahoo.com>
2023-01-20 12:43:02 -0600

    print script

--- 01-print.sh
+++ 01-print.sh
@@ -6,4 +6,4 @@ echo Hello
 # COL - COL CODE, 31-36 (31 - Red, 32 - Green, 33 - Yellow, 34 - Blue, 35 - Magenta, 36 - Cyan)
 # 0m - disables the enabled color
 
-echo "\e[31mHello in RED Color\e[0m"
+echo -e "\e[31mHello in RED Color\e[0m"
